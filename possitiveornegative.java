09324c283d6e4a48e7694a1ff9477a3cdc3d35f5
kaviarasanR <91777015+kaviarasanR@users.noreply.github.com>
2021-10-09 07:03:13 +0530

    Add files via upload

---
+++ possitiveornegative.java
@@ -0,0 +1,27 @@
+package javaweek1day1;
+
+public class possitiveornegative {
+
+	public static void main(String[] args) {
+		 int number = 54;
+		    
+		    if (number > 0)
+		    	
+		    {
+		    	System.out.println("Positive number is " + number);
+		    }
+		    
+		    else if (number < 0)
+		    	
+		    {
+		    	System.out.println("Negative number is" + number);
+		    }
+		    
+		    else {
+		    	
+		    	System.out.println("The number is neither positive nor negative");
+		    }
+
+	}
+
+}
