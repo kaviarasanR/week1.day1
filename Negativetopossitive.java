6cc21ea4553b0f527884b0b8a3eaf75ce30f1602
kaviarasanR <91777015+kaviarasanR@users.noreply.github.com>
2021-10-22 19:36:36 +0530

    Add files via upload

---
+++ Negativetopossitive.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Negativetopossitive {
+
+	public static void main(String[] args) {
+		int num = -40;
+		if (num < 0) {
+			num = num * -1;
+
+		}
+		System.out.println("The number -40 is converted to: " + num);
+	}
+
+}
